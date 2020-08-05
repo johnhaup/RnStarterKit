b1f8dd1db81f3ee0f4ea43678b34faf067603a00
johnhaup <johnhaup@gmail.com>
2020-08-04 20:53:19 -0700

    Add Auth0 google and email passwordless login.

--- src/screens/modals/__tests__/AModal-test.tsx
+++ src/screens/modals/__tests__/AModal-test.tsx
@@ -1,22 +1,26 @@
-/**
- * @format
- */
-
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { createStore, StoreProvider } from 'easy-peasy';
 import React from 'react';
 import 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import { Modals, ScreenParams } from '../../../navigation/types';
+import { storeModel } from '../../../store/models';
 import { AModal } from '../AModal';
 
 const navigation = {} as StackNavigationProp<ScreenParams, Modals>;
 const route = {} as RouteProp<ScreenParams, Modals>;
 
 it('renders correctly', () => {
+  const store = createStore(storeModel);
   const snap = renderer.create(
-    <AModal navigation={navigation} route={route} />,
+    <StoreProvider store={store}>
+      <SafeAreaProvider>
+        <AModal navigation={navigation} route={route} />
+      </SafeAreaProvider>
+    </StoreProvider>,
   );
   expect(snap).toMatchSnapshot();
 });
--- src/store/models/user.ts
+++ src/store/models/user.ts
@@ -1,23 +1,37 @@
 import { action, Action, persist } from 'easy-peasy';
+import { PasswordRealmResponse } from 'react-native-auth0';
 import { storage } from './../../services/storage';
+
+interface Auth extends PasswordRealmResponse {
+  fetchedAt: number; // Unix timestamp in seconds
+}
 
 interface UserState {
   onboardingComplete: boolean;
+  auth: Auth | null;
 }
 
 export interface UserModel extends UserState {
   setOnboardingComplete: Action<UserModel, boolean>;
+  setAuth: Action<UserModel, Auth | null>;
 }
 
 export const userModel: UserModel = persist(
   {
     // state
     onboardingComplete: false,
+    auth: null,
     // actions
     setOnboardingComplete: action(
       (state, onboardingComplete): UserState => ({
         ...state,
         onboardingComplete,
+      }),
+    ),
+    setAuth: action(
+      (state, auth): UserState => ({
+        ...state,
+        auth,
       }),
     ),
   },
